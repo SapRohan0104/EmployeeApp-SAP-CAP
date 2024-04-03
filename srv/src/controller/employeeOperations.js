f5b769ebfcbcb08eb9ce3f0d3c40b461f5a84205
Rohan Sharma <rohan.sharmasap0104@gmail.com>
2024-04-03 05:01:43 +0000

    02 April Class

--- srv/src/controller/employeeOperations.js
+++ srv/src/controller/employeeOperations.js
@@ -1,13 +1,23 @@
-
 const onBeforeEmployeeCreate = async (req) => {
-    console.log("Before Event is triggered");
-}
-
-const onAfterEmployeeCreate = async (req) => {
-    console.log("After Event is triggered");
-}
-
-module.exports = {
+    const { DOB } = req.data;
+    const iAge = _getAge(DOB);
+    req.data.age = iAge;
+  };
+  
+  const onAfterEmployeeCreate = async (req) => {
+    req.fName = `Mr. ${req.fName}`;
+    return req;
+  };
+  
+  const _getAge = (sDate) => {
+    const oToday = new Date(),
+      oBirthDate = new Date(sDate);
+    const age = oToday.getFullYear() - oBirthDate.getFullYear();
+    return age;
+  };
+  
+  module.exports = {
     onBeforeEmployeeCreate,
-    onAfterEmployeeCreate
-}
+    onAfterEmployeeCreate,
+  };
+  
